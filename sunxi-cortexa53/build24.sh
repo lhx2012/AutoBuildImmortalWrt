6649db17902c275b6d34f2f5ff02f64c81e238cb
lhx2012 <133630222+lhx2012@users.noreply.github.com>
2025-11-09 20:55:18 +0800

    Add new packages to build24.sh

--- sunxi-cortexa53/build24.sh
+++ sunxi-cortexa53/build24.sh
@@ -51,6 +51,8 @@ PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES script-utils"
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
